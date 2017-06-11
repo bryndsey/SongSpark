0186ccd2ad45675ea598c9b5e209c7fda2352334
Bryan Lindsey <blinz117@gmail.com>
2017-06-10 20:09:58 -0500

    Clean up formatting

--- app/src/main/java/com/bryndsey/songspark/ui/main/MainView.java
+++ app/src/main/java/com/bryndsey/songspark/ui/main/MainView.java
@@ -2,7 +2,10 @@ package com.bryndsey.songspark.ui.main;
 
 interface MainView {
 	void displaySong(String song);
+
 	void displayPlayingState();
+
 	void displayPausedState();
+
 	void disablePlayback();
 }
