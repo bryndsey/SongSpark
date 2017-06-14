339d677a30a06f210ac021d3dac27ab8887d580e
Bryan Lindsey <blinz117@gmail.com>
2017-06-14 07:25:03 -0500

    Add view model class for chords

---
+++ app/src/main/java/com/bryndsey/songspark/ui/chordview/ChordViewModel.java
@@ -0,0 +1,10 @@
+package com.bryndsey.songspark.ui.chordview;
+
+
+class ChordViewModel {
+	public final String chordName;
+
+	public ChordViewModel(String chordName) {
+		this.chordName = chordName;
+	}
+}
