e94d459ffa6d786d2c9932a8a1d9f7c507d5939b
Bryan Lindsey <blinz117@gmail.com>
2017-07-05 19:54:12 -0500

    Add open source license text
    -Use library which shows these in a dialog already. This is shown by clicking the extra button on the about page
    -Also attribute metronome icon

--- app/src/main/java/com/bryndsey/songspark/ui/menu/about/AboutFragment.java
+++ app/src/main/java/com/bryndsey/songspark/ui/menu/about/AboutFragment.java
@@ -1,15 +1,23 @@
 package com.bryndsey.songspark.ui.menu.about;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.bryndsey.songspark.R;
 import com.bryndsey.songspark.ui.base.BaseFragment;
 import com.metova.slim.annotation.Layout;
+
+import de.psdev.licensesdialog.LicensesDialog;
+import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20;
+import de.psdev.licensesdialog.licenses.MITLicense;
+import de.psdev.licensesdialog.model.Notice;
+import de.psdev.licensesdialog.model.Notices;
 
 @Layout(R.layout.empty)
 public class AboutFragment extends BaseFragment {
@@ -33,6 +41,22 @@ public class AboutFragment extends BaseFragment {
 					.title(R.string.about)
 					.customView(R.layout.about_page, true)
 					.positiveText("OK")
+					.neutralText("View Licenses")
+					.onNeutral(new MaterialDialog.SingleButtonCallback() {
+						@Override
+						public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+							final Notices notices = new Notices();
+							notices.addNotice(new Notice("Apache License 2.0", "http://www.apache.org/licenses/", "", new ApacheSoftwareLicense20()));
+							notices.addNotice(new Notice("MIT License", "https://opensource.org/licenses/MIT", "", new MITLicense()));
+
+							new LicensesDialog.Builder(getContext())
+									.setTitle("Open Source Licenses")
+									.setNotices(notices)
+									.setShowFullLicenseText(true)
+									.build()
+									.show();
+						}
+					})
 					.show();
 
 			return true;
