231dcc823211f20e2dc25d6770a03cad8f8ee348
Bryan Lindsey <blinz117@gmail.com>
2017-07-21 16:59:59 -0500

    Replace Random instance with RandomNumberGenerator class

--- songbuilder/src/main/java/com/bryndsey/songbuilder/SongGenerator.java
+++ songbuilder/src/main/java/com/bryndsey/songbuilder/SongGenerator.java
@@ -3,13 +3,11 @@ package com.bryndsey.songbuilder;
 import com.bryndsey.songbuilder.songstructure.ChordProgression;
 import com.bryndsey.songbuilder.songstructure.MusicStructure.Cadence;
 
-import java.util.Random;
-
 import javax.inject.Inject;
 
-public class SongGenerator {
+import static com.bryndsey.songbuilder.RandomNumberGenerator.getRandomDouble;
 
-	private final Random randGen;
+public class SongGenerator {
 
 	private ChordProgressionGenerator chordProgressionGenerator;
 
@@ -18,13 +16,12 @@ public class SongGenerator {
 	@Inject
 	SongGenerator(ChordProgressionGenerator chordProgressionGenerator) {
 		this.chordProgressionGenerator = chordProgressionGenerator;
-		randGen = new Random();
 		cadenceTransformer = new CadenceTransformer();
 	}
 
 	public ChordProgression generateVerseProgression() {
 		ChordProgression verseProgression = chordProgressionGenerator.generateChordProgression();
-		double cadenceChance = randGen.nextDouble();
+		double cadenceChance = getRandomDouble();
 		if (cadenceChance < 0.75)
 			cadenceTransformer.applyCadence(verseProgression.patterns.get(verseProgression.patterns.size() - 1), Cadence.HALF);
 		else if (cadenceChance < 0.8)
@@ -38,7 +35,7 @@ public class SongGenerator {
 	public ChordProgression generateChorusProgression() {
 		ChordProgression chorus = chordProgressionGenerator.generateChordProgression();
 		Cadence type = null;
-		double cadenceChance = randGen.nextDouble();
+		double cadenceChance = getRandomDouble();
 		if (cadenceChance < 0.35)
 			type = Cadence.AUTHENTIC;
 		else if (cadenceChance < 0.65)
