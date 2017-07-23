f65a39878c1258193672b3db4a258c7be0b042a6
Bryan Lindsey <blinz117@gmail.com>
2017-07-23 11:22:40 -0500

    Add arpeggios to chord instruments
    -Have a rnadom chance to use arpeggios instead of a pattern-based set of triads
    - Also have a random chance to do 1 or 2 arpeggios per chord

--- songbuilder/src/main/java/com/bryndsey/songbuilder/songgeneration/ChordNoteGenerator.java
+++ songbuilder/src/main/java/com/bryndsey/songbuilder/songgeneration/ChordNoteGenerator.java
@@ -1,29 +1,35 @@
 package com.bryndsey.songbuilder.songgeneration;
 
 
+import com.bryndsey.songbuilder.SongGenerationProperties;
 import com.bryndsey.songbuilder.songstructure.ChordProgression;
 import com.bryndsey.songbuilder.songstructure.Note;
 import com.bryndsey.songbuilder.songstructure.Pattern;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+
+import static com.bryndsey.songbuilder.RandomNumberGenerator.getRandomDouble;
 
 @Singleton
 public class ChordNoteGenerator {
 
 	private final RhythmGenerator rhythmGenerator;
+	private final SongGenerationProperties songGenerationProperties;
 
 	@Inject
-	ChordNoteGenerator(RhythmGenerator rhythmGenerator) {
+	ChordNoteGenerator(RhythmGenerator rhythmGenerator,
+					   SongGenerationProperties songGenerationProperties) {
 		this.rhythmGenerator = rhythmGenerator;
+		this.songGenerationProperties = songGenerationProperties;
 	}
 
-	// TODO: Clean this up
 	void generateChordNotes(ChordProgression progression) {
 
-		ArrayList<Note> chordPattern = generateTriadChordNotes();
+		ArrayList<Note> chordPattern = chooseChordNotes();
 
 		for (Pattern pattern : progression.patterns) {
 			ArrayList<ArrayList<Note>> chordNotes = new ArrayList<>();
@@ -32,6 +38,17 @@ public class ChordNoteGenerator {
 			}
 			pattern.chordNotes = chordNotes;
 		}
+	}
+
+	private ArrayList<Note> chooseChordNotes() {
+		ArrayList<Note> value;
+		if (getRandomDouble() < 0.75) {
+			value = generateTriadChordNotes();
+		} else {
+			value = generateArpeggioChordNotes();
+		}
+
+		return value;
 	}
 
 	private ArrayList<Note> generateTriadChordNotes() {
@@ -55,6 +72,47 @@ public class ChordNoteGenerator {
 			noteList.add(new Note(1, currentBeat, length));
 
 			currentBeat += length;
+		}
+
+		return noteList;
+	}
+
+	private ArrayList<Note> generateArpeggioChordNotes() {
+		ArrayList<Note> noteList = new ArrayList<>();
+
+		int numberOfBeats = songGenerationProperties.getTimeSigNumerator();
+
+		List<Integer> pitchList = new ArrayList<>(numberOfBeats);
+
+		if (numberOfBeats == 2) {
+			pitchList.add(1);
+			pitchList.add(5);
+		} else if (numberOfBeats == 3) {
+			pitchList.add(1);
+			pitchList.add(3);
+			pitchList.add(5);
+		} else if (numberOfBeats == 4) {
+			pitchList.add(1);
+			pitchList.add(3);
+			pitchList.add(5);
+			pitchList.add(3);
+		}
+
+		int numberOfArpeggiosPerChord;
+		if (getRandomDouble() < 0.8) {
+			numberOfArpeggiosPerChord = 2;
+		} else {
+			numberOfArpeggiosPerChord = 1;
+		}
+
+		for (int beat = 0; beat < pitchList.size() * numberOfArpeggiosPerChord; beat++) {
+			int pitch = pitchList.get(beat % pitchList.size());
+			float startBeat = (float) beat / (float) numberOfArpeggiosPerChord;
+			float length = 1f / numberOfArpeggiosPerChord;
+
+
+			Note note = new Note(pitch, startBeat, length);
+			noteList.add(note);
 		}
 
 		return noteList;
