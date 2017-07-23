56d384a54c3560bfe6e324e0886b376874ab3eb7
Bryan Lindsey <blinz117@gmail.com>
2017-07-23 15:40:27 -0500

    Formatting and access level cleanup

--- songbuilder/src/main/java/com/bryndsey/songbuilder/songgeneration/RhythmGenerator.java
+++ songbuilder/src/main/java/com/bryndsey/songbuilder/songgeneration/RhythmGenerator.java
@@ -21,39 +21,42 @@ public class RhythmGenerator {
 	}
 
 	// TODO: Have this return a list of Notes that use start beats and lengths
-	public ArrayList<Integer> generateRhythm(int numUnitsPerBeat) {
+	ArrayList<Integer> generateRhythm(int numUnitsPerBeat) {
 		if (numUnitsPerBeat <= 0)
 			return null;
 
 		int numSubbeats = songGenerationProperties.getTimeSigNumerator() * numUnitsPerBeat;
-		ArrayList<Integer> rhythm = new ArrayList<Integer>();
+		ArrayList<Integer> rhythm = new ArrayList<>();
 
 		int note = 0;
 		while (note < numSubbeats) {
 			// generate even numbered notes with weird distribution
 			double[] probs = new double[numSubbeats - note];
 			for (int prob = 0; prob < probs.length; prob++) {
-				if (prob == 0)
+				if (prob == 0) {
 					probs[prob] = probs.length / songGenerationProperties.getEighthNoteFactor();
-				else if (/*prob == 0 ||*/ (prob + 1) % 2 == 0)
+				} else if (/*prob == 0 ||*/ (prob + 1) % 2 == 0) {
 					probs[prob] = (double) (probs.length - prob);
-				else
+				} else {
 					probs[prob] = 0;
+				}
 
 				if (note % 4 == 0) {
 					probs[0] = probs[0] / 3.0;
 				}
 			}
 			int numBeats = Utils.pickNdxByProb(probs) + 1;
-			if (numBeats <= 0)
+			if (numBeats <= 0) {
 				continue;
+			}
 
 			// small chance to be negative (a rest)
 			double restChance = getRandomDouble();
 			if ((note == 0 && restChance < 0.02) // first note
 					|| (note + numBeats >= numSubbeats && restChance < 0.08) //last note
-					|| restChance < 0.005)
+					|| restChance < 0.005) {
 				numBeats *= -1;
+			}
 
 			note += Math.abs(numBeats);
 
